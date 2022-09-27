bc10c3a3be105e3f02bade0d224eb9c1d86abfd8
Matt Quintaes <matheus.quintaes@gmail.com>
2022-09-27 20:16:04 +1300

    favicon

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,10 @@
 import { Link } from 'react-router-dom'
-import LogoSvg from '../LogoSvg'
 function Header() {
   return (
     <div className="bg-slate-50 shadow-md shadow-slate-900/5 flex justify-center">
       <div className="max-w-2xl w-full flex items-center px-4 py-4">
         <Link to={`/`}>
-          <LogoSvg />
+          <img src="/KiwiDrivingLogo.svg" alt="" />
         </Link>
       </div>
     </div>
