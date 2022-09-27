c472579d57b658462dd358637f4a787ec092907a
Matt Quintaes <matheus.quintaes@gmail.com>
2022-09-27 20:04:41 +1300

    Adding new logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,12 @@
 import { Link } from 'react-router-dom'
-
+import LogoSvg from '../LogoSvg'
 function Header() {
   return (
     <div className="bg-slate-50 shadow-md shadow-slate-900/5 flex justify-center">
-      <div className="max-w-2xl w-full flex items-center px-4 py-2">
+      <div className="max-w-2xl w-full flex items-center px-4 py-4">
         <Link to={`/`}>
-          <img className="mr-4" src={'../src/assets/logo.png'} alt="logo" />
+          <LogoSvg />
         </Link>
-        <p className="text-xs font-bold">Kiwi Driving Test</p>
       </div>
     </div>
   )
